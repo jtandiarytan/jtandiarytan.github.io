ee0763283be63e7b165a5022af31b79a629c8b11
Jeremy Tandiary <jttan3@illinois.edu>
2023-10-15 04:22:46 -0500

    Initialize pages and test import about

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import About from './pages/about'
 import './App.css';
 
 function App() {
@@ -7,18 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Tan Jeremy Tandiary</p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {About}
       </header>
     </div>
   );
