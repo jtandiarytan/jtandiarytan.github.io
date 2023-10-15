83ab76a34cefdf4ee06d2ba2e56c5b3118fce504
Jeremy Tandiary <jttan3@illinois.edu>
2023-10-15 04:25:36 -0500

    Initialize pages and first changes to template

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Tan Jeremy Tandiary</p>
-        {About}
+        <About />
       </header>
     </div>
   );
