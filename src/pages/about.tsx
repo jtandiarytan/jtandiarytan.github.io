15db7eb6b5be1e68532638653471e27e0c3df220
Jeremy Tandiary <jttan3@illinois.edu>
2023-10-16 02:38:08 -0500

    About Section

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 
 function About() {
   return (
-    <section id="projects">
-      <h2>Projects</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <div>
-        <h3>Project 1</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
-      <div>
-        <h3>Project 2</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
-    </section>
+    <div className="container">
+    <div className="about">
+        <div className="heading">
+            <h1>Tan Jeremy Tandiary</h1>
+            <h2>Software Engineer, Graduating from UIUC December 2023</h2>
+        </div>
+        <div className="purpose">
+            <p>lorem ipsum dolor sit amet, con</p>
+        </div>
+
+    </div>
+    </div>
   );
 }
 
